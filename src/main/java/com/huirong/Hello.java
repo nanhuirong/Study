9772804f71a147a0ed4a16f7a09b492cc8353e6e
nanhuirong <nanhuirong@gmail.com>
2017-03-02 11:19:16 +0800

    2017-03-02 Java集合类总结, 红黑树了解

---
+++ src/main/java/com/huirong/Hello.java
@@ -0,0 +1,12 @@
+package com.huirong;
+
+/**
+ * Created by huirong on 17-3-1.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        Object key = null;
+        System.out.println(key.hashCode());
+
+    }
+}
